1a264feb88fcfbdf493ea26b8c899bc0179cbfc0
Milan Aleksic <milanaleksic@gmail.com>
2016-04-02 17:38:10 +0200

    using overridable excludes to avoid warnings

--- metalinter.mk
+++ metalinter.mk
@@ -4,6 +4,8 @@
 
 include $(dir $(lastword $(MAKEFILE_LIST)))/metalinter_common.mk
 
+EXCLUDES_METALINTER := bindata_.*.go
+
 .PHONY: metalinter
 metalinter: ${APP_NAME}
-	gometalinter --exclude="bindata_*" --vendor --disable=gotype --deadline=10m ./...
+	gometalinter --exclude="${EXCLUDES_METALINTER}" --vendor --disable=gotype --deadline=10m ./...
