d88da100b61fbfc4613a9c3fa803cc2c378d8c09
Milan Aleksic <milanaleksic@gmail.com>
2016-07-31 11:38:33 +0200

    Simple way od deploying without depending on 3rd party services

---
+++ headless.mk
@@ -0,0 +1,19 @@
+# Depends on:
+#   APP_NAME - project name
+#   GOPATH
+#   TAG
+#   _deploy - real logic of deployment (a task)
+
+VERSION := $(shell git name-rev --tags --name-only `git rev-parse HEAD`)_$(shell git rev-parse --short HEAD)
+
+.PHONY: deploy
+deploy: metalinter
+ifndef TAG
+	$(error TAG parameter must be set: make TAG=<TAG_VALUE>)
+endif
+	echo Creating and pushing tag
+	git tag ${TAG}
+	git push --tags
+	echo Deploying...
+	$(MAKE) _deploy
+
