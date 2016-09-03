ea6c908d522c7137c1261880f70f0dba7da7eb40
Milan Aleksic <milanaleksic@gmail.com>
2016-09-03 17:53:49 +0200

    closes #9

--- githook_prepush.sh
+++ githook_prepush.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+while read line
+do
+  if [[ $line =~ refs/tags/ ]]; then
+    echo "Avoiding pre-push hook since we are pushing tags, not commits"
+    exit 0
+  fi
+done < /dev/stdin
+
 set -e
 
 echo "Executing pre-push script: goimports"
