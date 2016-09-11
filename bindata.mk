86b97aef93f46fc6b4a8d8ce13a0257bf59b0efb
Milan Aleksic <milanaleksic@gmail.com>
2016-09-11 18:41:00 +0200

    fixing generated code by bindata with goimports to avoid problems with pre-push

--- bindata.mk
+++ bindata.mk
@@ -18,10 +18,12 @@ ${GOPATH}/bin/go-bindata:
 ${BINDATA_DEBUG_FILE}: ${SOURCES_DATA}
 	rm -rf ${BINDATA_RELEASE_FILE}
 	go-bindata --debug -o=${BINDATA_DEBUG_FILE} ${DATA_DIR}/...
+	goimports -w ${BINDATA_DEBUG_FILE}
 
 ${BINDATA_RELEASE_FILE}: ${SOURCES_DATA}
 	rm -rf ${BINDATA_DEBUG_FILE}
 	go-bindata -nocompress=true -nomemcopy=true -o=${BINDATA_RELEASE_FILE} ${DATA_DIR}/...
+	goimports -w ${BINDATA_RELEASE_FILE}
 
 .PHONY: clean_bindata
 clean_bindata: 
