66fc1cc5de817351f53c668153c1ef75cb5df473
Milan Aleksic <milanaleksic@gmail.com>
2017-01-26 09:20:42 +0100

    utilizing new UPX from github project

--- upx.mk
+++ upx.mk
@@ -2,6 +2,7 @@
 #   GOPATH
 
 UPX_ARCH := $(shell if grep -q arm <(uname -m); then echo 'armeb'; else echo 'amd64'; fi)
+UPX_VERSION := 3.92
 
 .PHONY: prepare_upx
 prepare_upx: ${GOPATH}/bin/goupx \
@@ -13,9 +14,9 @@ upx:
 		ln -s $(shell which upx) upx; \
 	elif [ "${UPX_ARCH}" == "amd64" ]; \
 	then \
-		curl http://upx.sourceforge.net/download/upx-3.91-${UPX_ARCH}_linux.tar.bz2 | tar xjvf - && \
-		mv upx-3.91-${UPX_ARCH}_linux/upx upx && \
-		rm -rf upx-3.91-${UPX_ARCH}_linux; \
+		curl -L https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-${UPX_ARCH}_linux.tar.xz | tar xJvf - && \
+		mv upx-${UPX_VERSION}-${UPX_ARCH}_linux/upx upx && \
+		rm -rf upx-${UPX_VERSION}-${UPX_ARCH}_linux; \
 	else \
 		echo Please install UPX for your ARM server via apt-get or fix Makefile to work with your system; \
 		exit 1; \
