537ae92c2fd1913d4a443dcc4d21dd0f782e4393
Milan Aleksic <milanaleksic@gmail.com>
2017-12-03 19:03:53 +0100

    removing the need to 'prepare' for metalinter

--- metalinter.mk
+++ metalinter.mk
@@ -4,19 +4,17 @@
 #   RESOURCES_DIR - where is bindata resources directory (where are bindata GO files generated)
 
 EXCLUDES_METALINTER := ($(RESOURCES_DIR)/bindata_.*.go)|(.*_test.go)
-EXTRA_ARGS_METALINTER := 
+EXTRA_ARGS_METALINTER :=
+GOMETALINTER := ${GOPATH}/bin/gometalinter
 
-.PHONY: prepare_metalinter
-prepare_metalinter: ${GOPATH}/bin/gometalinter
-
-${GOPATH}/bin/gometalinter:
-	go get github.com/alecthomas/gometalinter
-	gometalinter --install --update
+$(GOMETALINTER):
+	go get -u github.com/alecthomas/gometalinter
+	$(GOMETALINTER) --install --update
 
 .PHONY: metalinter
-metalinter: $(SOURCES)
-	gometalinter --exclude="${EXCLUDES_METALINTER}" --vendor --disable=gotype --deadline=10m ${EXTRA_ARGS_METALINTER} ./... | sed "s/^/[METALINTER_WARN] /" || true
+metalinter: $(SOURCES) | $(GOMETALINTER)
+	$(GOMETALINTER) --exclude="${EXCLUDES_METALINTER}" --vendor --disable=gotype --deadline=10m ${EXTRA_ARGS_METALINTER} ./... | sed "s/^/[METALINTER_WARN] /" || true
 
 .PHONY: metalinter_strict
-metalinter_strict: $(SOURCES)
-	gometalinter --exclude="${EXCLUDES_METALINTER}" --vendor --disable=gotype --deadline=10m ${EXTRA_ARGS_METALINTER} ./...
+metalinter_strict: $(SOURCES) | $(GOMETALINTER)
+	$(GOMETALINTER) --exclude="${EXCLUDES_METALINTER}" --vendor --disable=gotype --deadline=10m ${EXTRA_ARGS_METALINTER} ./...
