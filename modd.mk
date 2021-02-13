7a02dd69c2a4bc0dd591329c0a25e60dee72e9af
Milan Aleksić <milanaleksic@gmail.com>
2021-02-13 16:54:18 +0100

    get devd when getting motdd

--- modd.mk
+++ modd.mk
@@ -2,11 +2,14 @@
 #   GOPATH
 #   modd.conf file in the project root
 
-MODD := ${GOPATH}/bin/modd
+DEVD := ${GOPATH}/bin/devd
+$(DEVD):
+	go get github.com/milanaleksic/devd/cmd/devd@v1.0.2
 
+MODD := ${GOPATH}/bin/modd
 $(MODD):
 	go get github.com/cortesi/modd
 
 .PHONY: dev
-dev:
+dev: $(DEVD)
 	$(MODD)
