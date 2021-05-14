365025b262270fd5b85c9e05007a04a77585b0ce
Milan Aleksić <milanaleksic@gmail.com>
2021-05-14 17:04:46 +0200

    chore: fix a typo

--- resources-embed.mk
+++ resources-embed.mk
@@ -12,5 +12,5 @@ SOURCES_DATA := $(shell find $(DATA_DIR))
 RESOURCES_SIGNAL_GO ?= $(RESOURCES_DIR)/signal.go
 
 ${RESOURCES_SIGNAL_GO}: $(SOURCES_DATA)
-	@echo sinaling that changes have ocurred by updating signal.go
+	@echo Signaling that resource changes have ocurred by updating signal.go
 	@echo "package $$(basename `dirname ${RESOURCES_SIGNAL_GO}`)" > ${RESOURCES_SIGNAL_GO}
