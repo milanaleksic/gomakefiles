7c17d5b5ecaa2061ac24a9cbab0cad62471fb883
Milan Aleksić <milanaleksic@gmail.com>
2021-07-11 07:55:24 +0000

    add wire.mk

---
+++ wire.mk
@@ -0,0 +1,10 @@
+# Depends on:
+#   APP_NAME - project (github) name
+#   GOPATH
+#   RESOURCES_DIR - where is bindata resources directory (where are bindata GO files generated)
+
+WIRE := ${GOPATH}/bin/wire
+
+$(WIRE):
+	@echo "wire NOT FOUND: $(WIRE)"
+	@go get -u github.com/google/wire/cmd/wire
