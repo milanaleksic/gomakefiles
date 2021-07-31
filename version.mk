ee723657475142c751f32e8026b1535af969636f
Milan Aleksić <milanaleksic@gmail.com>
2021-07-31 14:51:00 +0200

    github support: version extraction

---
+++ version.mk
@@ -0,0 +1,8 @@
+IS_DEFINED_VERSION := $(shell [ ! "${VERSION}" == "" ] && echo true)
+ifneq ($(IS_DEFINED_VERSION),true)
+VERSION := $(shell git name-rev --tags --name-only `git rev-parse HEAD`)
+endif
+
+ifeq ($(IS_DEFINED_VERSION),true)
+IS_REAL_VERSION := $(shell [ ! "${VERSION}" == "undefined" ] && echo true)
+endif
