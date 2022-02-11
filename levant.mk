add445ba8caf6cd845475ae48ff1ab66a1393f3f
Milan Aleksić <milanaleksic@gmail.com>
2022-02-11 20:34:40 +0100

    introduce CD via levant

---
+++ levant.mk
@@ -0,0 +1,10 @@
+LEVANT := $(SOURCEDIR)/levant
+$(LEVANT):
+	@echo "installing 'levant' executable: $(LEVANT) version $(LEVANT_VERSION)"
+	@curl -L -O https://releases.hashicorp.com/levant/$(LEVANT_VERSION)/levant_$(LEVANT_VERSION)_linux_amd64.zip
+	@unzip levant_$(LEVANT_VERSION)_linux_amd64.zip
+	@mv levant $(LEVANT)
+
+.PHONY: cd-levant
+cd-levant: $(LEVANT)
+	$(LEVANT) .github/workflows/thoughttrain.nomad
