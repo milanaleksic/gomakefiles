b79ce8cd5a9f9f5c822529dc7ac1f56c226be410
Milan Aleksić <milanaleksic@gmail.com>
2022-02-15 20:08:33 +0100

    levant setup variable usage fix

--- levant.mk
+++ levant.mk
@@ -4,9 +4,20 @@ $(LEVANT):
 	@curl -L -O https://releases.hashicorp.com/levant/$(LEVANT_VERSION)/levant_$(LEVANT_VERSION)_linux_amd64.zip
 	@unzip levant*.zip && chmod +x levant
 
+ifdef LEVANT_VAR_FILE
+LEVANT_ARG_VAR_FILE := "-var-file=$(LEVANT_VAR_FILE)"
+else
+LEVANT_ARG_VAR_FILE := ""
+endif
+ifdef LEVANT_CONSUL_LOCATION
+LEVANT_ARG_CONSUL_ADDRESS := "-consul-address=$(LEVANT_CONSUL_LOCATION)"
+else
+LEVANT_ARG_CONSUL_ADDRESS := ""
+endif
+
 .PHONY: cd-levant
 cd-levant: $(LEVANT)
 ifndef LEVANT_TARGET
 	$(error LEVANT_TARGET parameter must be set: make LEVANT_TARGET=<LEVANT_TARGET_VALUE>)
 endif
-	$(LEVANT) deploy -var-file=$(LEVANT_VAR_FILE) $(LEVANT_TARGET)
+	@$(LEVANT) $(LEVANT_ARG_VAR_FILE) $(LEVANT_ARG_CONSUL_ADDRESS) deploy $(LEVANT_TARGET)
