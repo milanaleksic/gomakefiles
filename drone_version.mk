8084243b9bd703383cf00fa06781a1de1ead9f4f
Milan Aleksić <milanaleksic@gmail.com>
2022-03-22 17:35:50 +0100

    when on DroneCI, do not parse git state - depend on DRONE_TAG env variable

--- drone_version.mk
+++ drone_version.mk
@@ -3,11 +3,6 @@ ifneq ($(IS_DEFINED_VERSION),true)
 VERSION := ${DRONE_TAG}
 endif
 
-IS_DEFINED_VERSION := $(shell [ ! "${VERSION}" == "" ] && echo true)
-ifneq ($(IS_DEFINED_VERSION),true)
-VERSION := $(shell git name-rev --tags --name-only `git rev-parse HEAD`)
-endif
-
 ifeq ($(IS_DEFINED_VERSION),true)
 IS_REAL_VERSION := $(shell [ ! "${VERSION}" == "undefined" ] && echo true)
 endif
