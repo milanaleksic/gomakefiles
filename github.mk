0dc650665e52abc3ea045b9467f83b4c6e513cb5
Milan Aleksić <milanaleksic@gmail.com>
2022-08-16 08:18:39 +0200

    install goreleaser from source, like other tools and remove bastion

--- github.mk
+++ github.mk
@@ -6,28 +6,17 @@
 #   PACKAGE - what is the full package name for this go application?
 #   MAIN_APP_DIR - what is the location from this Makefile of the main package that we consider main deployment artifact?
 
-GORELEASER := $(SOURCEDIR)/goreleaser
+GORELEASER := ${GOPATH}/bin/goreleaser
 
 $(GORELEASER):
-	@echo "downloading 'goreleaser' executable: $(GORELEASER) version $(GORELEASER_VERSION)"
-	@curl -LO https://github.com/goreleaser/goreleaser/releases/download/$(GORELEASER_VERSION)/goreleaser_Linux_x86_64.tar.gz
-	@tar xzvf goreleaser_Linux_x86_64.tar.gz goreleaser
-	@rm -rf goreleaser_Linux_x86_64.tar.gz
+	@echo "downloading 'goreleaser' executable version $(GORELEASER_VERSION)"
+	@go install github.com/goreleaser/goreleaser@$(GORELEASER_VERSION)
 
 .PHONY: release-local
-release-local:
+release-local: $(GORELEASER)
 	$(GORELEASER) release --snapshot --skip-publish --rm-dist
 	$(GORELEASER) build --single-target
 
 .PHONY: ci
 ci: $(GORELEASER)
 	$(GORELEASER) release
-
-.PHONY: cd
-cd:
-ifndef BASTION_TOKEN
-	$(error BASTION_TOKEN parameter must be set: make BASTION_TOKEN=<BASTION_TOKEN_VALUE>)
-endif
-ifeq ($(IS_REAL_VERSION),true)
-	curl --fail -X POST https://misc.milanaleksic.net/bastion/deploy?value=${VERSION} --header 'Authorization: Token ${BASTION_TOKEN}'
-endif
