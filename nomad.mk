20575c971cc8e392d09a65128c9963c92a168b91
Milan Aleksić <milanaleksic@gmail.com>
2023-03-18 11:11:15 +0100

    introduce nomad support

---
+++ nomad.mk
@@ -0,0 +1,18 @@
+NOMAD := $(SOURCEDIR)/nomad
+$(NOMAD):
+	@echo "installing 'nomad' executable: $(NOMAD) version $(NOMAD_VERSION)"
+	@curl -L -O https://releases.hashicorp.com/nomad/$(NOMAD_VERSION)/nomad_$(NOMAD_VERSION)_linux_amd64.zip
+	@unzip nomad*.zip && chmod +x nomad
+
+.PHONY: cd-nomad
+cd-nomad: $(NOMAD)
+ifndef NOMAD_TARGET
+	$(error NOMAD_TARGET parameter must be set: make NOMAD_TARGET=<NOMAD_TARGET_VALUE>)
+endif
+ifeq ($(IS_REAL_VERSION),true)
+	export $(NOMAD) deploy \
+			-log-level=WARN \
+			-var image_version=${VERSION} \
+			$(NOMAD_EXTRA_ARGS) \
+			$(NOMAD_TARGET)
+endif
