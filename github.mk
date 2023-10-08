d75959c9dcd8598f0bf1d0e46b30854adf3686bb
Milan Aleksić <milanaleksic@gmail.com>
2023-10-08 11:45:32 +0200

    fix for goreleaser command

--- github.mk
+++ github.mk
@@ -15,7 +15,7 @@ $(GORELEASER):
 		echo "using archive $${TAR_FILE}" && \
 		curl -sfLo "$${TAR_FILE}" \
 		  "https://github.com/goreleaser/goreleaser/releases/download/$(GORELEASER_VERSION)/goreleaser_$$(uname -s)_$$(uname -m).tar.gz"  && \
-		tar -xf "$${TAR_FILE}" -C $TMPDIR goreleaser && \
+		tar -xf "$${TAR_FILE}" -C $$TMPDIR goreleaser && \
 		mv $${TMPDIR}/goreleaser $(GORELEASER)
 
 .PHONY: release-local
