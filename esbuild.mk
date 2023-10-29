9d2c0a88000ee487dab9eb4e59509f442e6fc619
Milan Aleksić <milanaleksic@gmail.com>
2023-10-29 09:52:43 +0100

    move esbuild into tools dir

--- esbuild.mk
+++ esbuild.mk
@@ -1,8 +1,9 @@
 # Depends on:
 #   GOPATH
 
-ESBUILD := ${GOPATH}/bin/esbuild
+ESBUILD := $(TOOLS_DIR)/esbuild
 
 $(ESBUILD):
 	@echo "installing 'esbuild' executable: $(ESBUILD) version $(ESBUILD_VERSION)"
 	go install github.com/evanw/esbuild/cmd/esbuild@$(ESBUILD_VERSION)
+	mv $$(which esbuild) $(ESBUILD)
