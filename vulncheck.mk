3b5c88b7e08a765e13f75cf2456ddb6bd4c195ca
Milan Aleksić <milanaleksic@gmail.com>
2024-03-02 10:07:30 +0100

    move govulncheck into self-managed flow

--- vulncheck.mk
+++ vulncheck.mk
@@ -1,14 +1,5 @@
-# Depends on:
-#   GOPATH
-
-GOVULNCHECK := ${GOPATH}/bin/govulncheck
-
-$(GOVULNCHECK):
-	@echo "installing 'govulncheck' executable: $(GOVULNCHECK)GOVULNCHECK"
-	@go install golang.org/x/vuln/cmd/govulncheck@latest
-
 .PHONY: govulncheck
-govulncheck: $(GOVULNCHECK)
+govulncheck:
 	@if [ -f go.work ]; \
 	then \
 		go work edit -json | jq -r '.Use[].DiskPath' | xargs -I{} bash -c 'govulncheck -v {}/... || true'; \
