ae74b1a51d3509e6fe19866cd59d1142e887a252
Milan Aleksić <milanaleksic@gmail.com>
2024-03-02 10:40:16 +0100

    remove golangcilint self-installation

--- golangcilint.mk
+++ golangcilint.mk
@@ -3,36 +3,29 @@
 #   GOPATH
 #   RESOURCES_DIR - where is bindata resources directory (where are bindata GO files generated)
 
-GOLANGCILINT_VERSION ?= v1.45.2
-LINTER := ${GOPATH}/bin/golangci-lint
-
-$(LINTER):
-	@echo "installing 'golangci-lint' executable: $(LINTER) version $(GOLANGCILINT_VERSION)"
-	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh |  sh -s -- -b $(GOPATH)/bin $(GOLANGCILINT_VERSION)
-
 .PHONY: metalinter
-metalinter: $(LINTER)
+metalinter:
 	@if [ -f go.work ]; \
 	then \
-		$(LINTER) run --issues-exit-code=0 --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
+		golangci-lint run --issues-exit-code=0 --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
 	else \
-		$(LINTER) run --issues-exit-code=0; \
+		golangci-lint run --issues-exit-code=0; \
 	fi
 
 .PHONY: metalinter_strict
-metalinter_strict: $(LINTER)
+metalinter_strict:
 	@if [ -f go.work ]; \
 	then \
-		$(LINTER) run --issues-exit-code=1 --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
+		golangci-lint run --issues-exit-code=1 --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
 	else \
-		$(LINTER) run --issues-exit-code=1; \
+		golangci-lint run --issues-exit-code=1; \
 	fi
 
 .PHONY: metalinter_fix
-metalinter_fix: $(LINTER)
+metalinter_fix:
 	@if [ -f go.work ]; \
 	then \
-		$(LINTER) run --issues-exit-code=1 --fix --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
+		golangci-lint run --issues-exit-code=1 --fix --out-format=github-actions -- $$(go work edit -json | jq -c -r '[.Use[].DiskPath] | map_values(. + "/...")[]'); \
 	else \
-		$(LINTER) run --issues-exit-code=1 --fix; \
+		golangci-lint run --issues-exit-code=1 --fix; \
 	fi
