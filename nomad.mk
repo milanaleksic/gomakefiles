c384fef72057cb5450c702a7e4c5a0ed30c88173
Milan Aleksić <milanaleksic@gmail.com>
2024-08-26 09:09:50 +0200

    fix for cd-nomad task

--- nomad.mk
+++ nomad.mk
@@ -4,7 +4,7 @@ ifndef NOMAD_TARGET
 	$(error NOMAD_TARGET parameter must be set: make NOMAD_TARGET=<NOMAD_TARGET_VALUE>)
 endif
 ifeq ($(IS_REAL_VERSION),true)
-	$(NOMAD) run \
+	nomad run \
 			-var image_version=${VERSION} \
 			$(NOMAD_EXTRA_ARGS) \
 			$(NOMAD_TARGET)
