5965ed843d961f2e8b8023b4d7fd75a758b2a906
Milan Aleksić <milanaleksic@gmail.com>
2024-09-21 13:06:21 +0200

    smaller notification when commit succeeds

--- githook_postcommit.sh
+++ githook_postcommit.sh
@@ -5,25 +5,7 @@ DATE=$(git log -1 --format=%cd --date=format:"%Y%m%d")
 
 git tag -a $DATE-$COMMIT -m "Tagging $DATE-$COMMIT"
 
-echo "                                         "
-echo "                   |ZZzzz                "
-echo "                   |                     "
-echo "                   |                     "
-echo "      |ZZzzz      /^\            |ZZzzz  "
-echo "      |          |~~~|           |       "
-echo "      |        |-     -|        / \      "
-echo "     /^\       |[]+    |       |^^^|     "
-echo "  |^^^^^^^|    |    +[]|       |   |     "
-echo "  |    +[]|/\/\/\/\^/\/\/\/\/|^^^^^^^|   "
-echo "  |+[]+   |~~~~~~~~~~~~~~~~~~|    +[]|   "
-echo "  |       |  []   /^\   []   |+[]+   |   "
-echo "  |   +[]+|  []  || ||  []   |   +[]+|   "
-echo "  |[]+    |      || ||       |[]+    |   "
-echo "  |_______|------------------|_______|   "
-echo "                                         "
-echo "                                         "
-echo "      You have just committed and tagged "
-echo "      your code as $DATE-$COMMIT         "
+echo "Code commited and tagged as $DATE-$COMMIT"
 
 echo "Removing deprecated un-pushed tags..."
 git show-ref --tags | \
