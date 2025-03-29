dd7407d5ac9dd33de7959ad3152c89fc089f8fd2
Milan Aleksić <milanaleksic@gmail.com>
2025-03-29 09:09:15 +0100

    fix govulncheck in go 1.24

--- vulncheck.mk
+++ vulncheck.mk
@@ -2,7 +2,7 @@
 govulncheck:
 	@if [ -f go.work ]; \
 	then \
-		go work edit -json | jq -r '.Use[].DiskPath' | xargs -I{} bash -c 'echo "running govulncheck on {}" && govulncheck -show verbose {}/... || true'; \
+		go work edit -json | jq -r '.Use[].DiskPath' | xargs -I{} bash -c 'echo "running govulncheck on {}" && cd {} && govulncheck -show verbose || true'; \
 	else \
-		govulncheck -show verbose $(SOURCEDIR)/... || true; \
+		govulncheck -show verbose || true; \
 	fi
